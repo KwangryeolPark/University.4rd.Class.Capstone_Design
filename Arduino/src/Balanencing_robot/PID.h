c8d68bd19cdec7ff4fc4bc1b79b4c3eb59b70f46
KwangryeolPark <pkr7098@gmail.com>
2022-06-09 20:21:30 +0900

    update

---
+++ Arduino/src/Balanencing_robot/PID.h
@@ -0,0 +1,49 @@
+#ifndef _PID_H
+#define _PID_H
+
+typedef double PID_TYPE;
+
+#define Kc  90.0
+#define Tc  0.45
+/*
+const PID_TYPE P_gain = 90 * 0.6;
+const PID_TYPE I_gain = P_gain * 2 / Tc;//0.9;
+const PID_TYPE D_gain = P_gain * Tc * 0.125;//43;
+*/
+
+const PID_TYPE P_gain = 70;
+const PID_TYPE I_gain = 200;//0.9;
+const PID_TYPE D_gain = 1;//43;
+
+class PID {
+  public:
+    PID();
+    void PID_cal(PID_TYPE error, PID_TYPE* pid, PID_TYPE gap);
+    void PID_vals(PID_TYPE* p, PID_TYPE* i, PID_TYPE* d);
+
+  private:
+    PID_TYPE  previous_error;
+    PID_TYPE  I_temp;
+    PID_TYPE  P_control, I_control, D_control;
+};
+
+PID::PID() {
+  previous_error = 0;
+  I_temp = 0;
+};
+
+void PID::PID_cal(PID_TYPE error, PID_TYPE* pid, PID_TYPE gap) {
+  I_temp += (error * gap);
+  *pid = (P_control = P_gain * error) + (I_control = I_gain * (I_temp)) + (D_control = D_gain * ((error - previous_error) / gap));
+  previous_error = error;
+};
+
+void PID::PID_vals(PID_TYPE* p, PID_TYPE* i, PID_TYPE* d) {
+  *p = P_control;
+  *i = I_control;
+  *d = D_control;
+}
+
+
+
+#endif
---
+++ Arduino/src/Balanencing_robot/motor.h
@@ -0,0 +1,40 @@
+#ifndef _MOTOR_H
+#define _MOTOR_H
+
+#define POWER_MOTOR
+
+class Motor {
+  public:
+    Motor(uint8_t A, uint8_t B);
+    void wheel(int16_t spd);
+
+  private:
+    uint8_t _A, _B;
+};
+
+Motor::Motor(uint8_t A, uint8_t B) {
+  pinMode(_A = A, OUTPUT);
+  pinMode(_B = B, OUTPUT);
+};
+
+void Motor::wheel(int16_t spd) {
+#ifdef  POWER_MOTOR
+  if (spd >= 0) {
+    analogWrite(_A, 255);
+    analogWrite(_B, 255 - spd);
+  } else {
+    analogWrite(_A, 255 + spd);
+    analogWrite(_B, 255);
+  }
+#else
+  if (spd >= 0) {
+    analogWrite(_A, spd);
+    analogWrite(_B, 0);
+  } else {
+    analogWrite(_A, 0);
+    analogWrite(_B, spd);
+  }
+#endif
+};
+
+#endif
